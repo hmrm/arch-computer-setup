22bb9da8d072864f6a05d33adedf0903d104d3fd
Haney Maxwell <hmrmaxwell@gmail.com>
2014-02-02 22:50:27 -0800

    adding java

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,4 +3,5 @@ node default {
   include rtorrent
   include dns
   include chrome
+  include java
 }
---
+++ modules/java/manifests/init.pp
@@ -0,0 +1,24 @@
+class java {
+  class { 'apt': }
+
+  apt::ppa { 'ppa:webupd8team/java': }
+
+  package { 'oracle-java6-installer':
+    ensure => latest,
+    before => Package['oracle-java7-set-default'],
+  }
+
+  package { 'oracle-java7-installer':
+    ensure => latest,
+    before => Package['oracle-java7-set-default'],
+  }
+
+  package { 'oracle-java8-installer':
+    ensure => latest,
+    before => Package['oracle-java7-set-default'],
+  }
+
+  package { 'oracle-java7-set-default':
+    ensure => latest,
+  }
+}
