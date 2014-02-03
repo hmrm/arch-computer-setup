e5e4da30bfe2fe558f6fa543a960b459ba5ef91d
Haney Maxwell <hmrmaxwell@gmail.com>
2014-02-02 19:29:26 -0800

    Adding utilities installation!

---
+++ modules/utilities/manifests/init.pp
@@ -0,0 +1,217 @@
+class utilities {
+  package { 'mutt':
+    ensure => latest,
+  }
+
+  package { 'mailutils':
+    ensure => latest,
+  }
+
+  package { 'curl':
+    ensure => latest,
+  }
+
+  package { 'wget':
+    ensure => latest,
+  }
+
+  package { 'dpkg':
+    ensure => latest,
+  }
+
+  package { 'gnome-shell':
+    ensure => latest,
+  }
+
+  package { 'ack-grep':
+    ensure => latest,
+  }
+
+  package { 'graphviz':
+    ensure => latest,
+  }
+
+  package { 'w3m':
+    ensure => latest,
+  }
+
+  package { 'build-essential':
+    ensure => latest,
+  }
+
+  package { 'gcc':
+    ensure => latest,
+  }
+
+  package { 'libc6-dev':
+    ensure => latest,
+  }
+
+  package { 'autoconf':
+    ensure => latest,
+  }
+
+  package { 'sqlite3':
+    ensure => latest,
+  }
+
+  package { 'm4':
+    ensure => latest,
+  }
+
+  package { 'libncurses-dev':
+    ensure => latest,
+  }
+
+  package { 'openssl':
+    ensure => latest,
+  }
+
+  package { 'flex':
+    ensure => latest,
+  }
+
+  package { 'bison':
+    ensure => latest,
+  }
+
+  package { 'xsltproc':
+    ensure => latest,
+  }
+
+  package { 'fop':
+    ensure => latest,
+  }
+
+  package { 'libwxgtk2.8-dev':
+    ensure => latest,
+  }
+
+  package { 'libgl1-mesa-dev':
+    ensure => latest,
+  }
+
+  package { 'libglu1-mesa-dev':
+    ensure => latest,
+  }
+
+  package { 'libpng3':
+    ensure => latest,
+  }
+
+  package { 'aptitude':
+    ensure => latest,
+  }
+
+  package { 'clang':
+    ensure => latest,
+  }
+
+  package { 'g++':
+    ensure => latest,
+  }
+
+  package { 'gfortran':
+    ensure => latest,
+  }
+
+  package { 'open-cobol':
+    ensure => latest,
+  }
+
+  package { 'texlive':
+    ensure => latest,
+  }
+
+  package { 'automake':
+    ensure => latest,
+  }
+
+  package { 're2c':
+    ensure => latest,
+  }
+
+  package { 'libtool':
+    ensure => latest,
+  }
+
+  package { 'php5':
+    ensure => latest,
+  }
+
+  package { 'r-base-dev':
+    ensure => latest,
+  }
+
+  package { 'htop':
+    ensure => latest,
+  }
+
+  package { 'gnugo':
+    ensure => latest,
+  }
+
+  package { 'libyaml-0-2':
+    ensure => latest,
+  }
+
+  package { 'git-core':
+    ensure => latest,
+  }
+
+  package { 'libyaml-dev':
+    ensure => latest,
+  }
+
+  package { 'libsqlite3-dev':
+    ensure => latest,
+  }
+
+  package { 'libxml2-dev':
+    ensure => latest,
+  }
+
+  package { 'libxslt1-dev':
+    ensure => latest,
+  }
+
+  package { 'libgdbm-dev':
+    ensure => latest,
+  }
+
+  package { 'iptraf':
+    ensure => latest,
+  }
+
+  package { 'markdown':
+    ensure => latest,
+  }
+
+  package { 'pandoc':
+    ensure => latest,
+  }
+
+  package { 'valgrind':
+    ensure => latest,
+  }
+
+  package { 'nmap':
+    ensure => latest,
+  }
+
+  package { 'alarm-clock':
+    ensure => latest,
+  }
+
+  package { 'zsh':
+    ensure => latest,
+  }
+
+  package { 'bash-completion':
+    ensure => latest,
+  }
+
+  package { 'sl':
+    ensure => latest,
+  }
+}
