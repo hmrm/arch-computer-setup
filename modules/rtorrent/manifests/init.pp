fbf4d5ca827e4d400138dc77002a41f0f096bd67
Haney Maxwell <hmrmaxwell@gmail.com>
2014-02-23 11:16:18 -0800

    systemdization of rtorrent

--- modules/rtorrent/manifests/init.pp
+++ modules/rtorrent/manifests/init.pp
@@ -1,4 +1,24 @@
 class rtorrent {
+  $user = hiera('user')
+
+  file { '/etc/systemd/system/rtorrent.service':
+    mode    => '644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('rtorrent/rtorrent.service.erb'),
+    notify  => Service['rtorrent'],
+  }
+
+  service { 'rtorrent':
+    ensure  => running,
+    enable  => true,
+    require => [
+                File['/etc/systemd/system/rtorrent.service'],
+                Package['rtorrent'],
+                Package['tmux'],
+                ],
+  }
+
   package { 'rtorrent':
     ensure => latest,
   }
