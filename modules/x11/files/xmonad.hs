f7a41e0297c4bd9e0254e19fa35d167ef9f2e8aa
Haney Maxwell <hmrmaxwell@gmail.com>
2014-02-23 20:50:29 -0800

    Adding x11 setup

---
+++ modules/x11/files/xmonad.hs
@@ -0,0 +1,61 @@
+import XMonad
+import XMonad.Layout.NoBorders
+
+-- LAYOUTS
+import XMonad.Layout.Spacing
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Grid
+
+
+
+--------------------------------------------------------------------------------------------------------------------
+-- DECLARE WORKSPACES RULES
+--------------------------------------------------------------------------------------------------------------------
+myLayout = tiledSpace ||| grid ||| tiled
+	where
+		tiled  		= spacing 5 $ ResizableTall nmaster delta ratio []
+		tiledSpace  	= spacing 60 $ ResizableTall nmaster delta ratio []
+                grid            = spacing 20 $ Grid
+		-- Default number of windows in master pane
+		nmaster = 1
+		-- Percent of the screen to increment when resizing
+		delta 	= 5/100
+		-- Default proportion of the screen taken up by main pane
+		ratio 	= toRational (2/(1 + sqrt 5 :: Double)) 
+
+
+--------------------------------------------------------------------------------------------------------------------
+-- WORKSPACE DEFINITIONS
+--------------------------------------------------------------------------------------------------------------------
+myWorkspaces = ["1"]
+
+main = do
+	xmonad $ defaultConfig
+		{ terminal              = myTerminal
+                , borderWidth		= 1
+		, layoutHook 		= smartBorders $ myLayout
+		, workspaces 		= myWorkspaces
+                , normalBorderColor     = color8
+                , focusedBorderColor    = color8
+		}
+
+myTerminal 	= "urxvtc"
+
+background= "#181512"
+foreground= "#D6C3B6"
+color0=  "#332d29"
+color8=  "#817267"
+color1=  "#8c644c"
+color9=  "#9f7155"
+color2=  "#746C48"
+color10= "#9f7155"
+color3=  "#bfba92"
+color11= "#E0DAAC"
+color4=  "#646a6d"
+color12= "#777E82"
+color5=  "#766782"
+color13= "#897796"
+color6=  "#4B5C5E"
+color14= "#556D70"
+color7=  "#504339"
+color15= "#9a875f"
---
+++ modules/x11/manifests/init.pp
@@ -0,0 +1,71 @@
+class x11 {
+  $user = hiera('user')
+
+  package { 'xorg-xrdb':
+    ensure => latest,
+  }
+
+  package { 'xorg-server':
+    ensure => latest,
+  }
+
+  package { 'xorg-xsetroot':
+    ensure => latest,
+  }
+
+  package { 'rxvt-unicode':
+    ensure => latest,
+  }
+
+  package { 'xmonad':
+    ensure => latest,
+  }
+
+  package { 'xmonad-contrib':
+    ensure => latest,
+  }
+
+  package { 'feh':
+    ensure => latest,
+  }
+
+  package { 'compton-git':
+    ensure  => latest,
+    require => Package['python2'], # for some reason, this dependency wasn't being resolved for me properly
+  }
+
+  file { "/home/${user}/.xinitrc":
+    mode => '755',
+    owner => $user,
+    group => 'wheel',
+    source => 'puppet:///module/x11/DOTxinitrc',
+  }
+
+  file { "/home/${user}/.xmonad":
+    mode => '755',
+    ensure => directory,
+    owner => $user,
+    group => 'wheel',
+  }
+
+  file { "/home/${user}/.xmonad/xmonad.hs":
+    mode => '755',
+    owner => $user,
+    group => 'wheel',
+    source => 'puppet:///modules/x11/xmonad.hs',
+  }
+
+  file { "/home/${user}/.Xresources":
+    mode => '755',
+    owner => $user,
+    group => 'wheel',
+    source => 'puppet:///modules/x11/DOTXresources'
+  }
+
+  file { "/home/${user}/.xmodmap":
+    mode => '755',
+    owner => $user,
+    group => 'wheel',
+    source => 'puppet:///module/x11/DOTxmodmap',
+  }
+}
