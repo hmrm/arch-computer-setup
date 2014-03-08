2f6b72da0364926e0a3bd7577776519e8b2ca17b
Haney Maxwell <hmrmaxwell@gmail.com>
2014-03-08 12:30:43 -0800

    Fixing some of rvm

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -2,9 +2,11 @@ class ruby {
   $user = hiera('user')
 
   exec { 'install rvm':
-    command => '\curl -L https://get.rvm.io | bash -s stable --ruby; source ~/.rvm/scripts/rvm; rvm pkg install openssl; rvm reinstall all --force;',
+    command => 'curl -L https://get.rvm.io | bash -s stable --ruby; source ~/.rvm/scripts/rvm; rvm pkg install openssl; rvm reinstall all --force;',
     path => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
     creates => "/home/${user}/.rvm",
+    user => $user,
+    environment => ["HOME=/home/${user}/"],
     require => Package['curl'],
   }
 }
