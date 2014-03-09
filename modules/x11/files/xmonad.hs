75291325a4a32985ba815f3035ed883ea838776a
Haney Maxwell <hmrmaxwell@gmail.com>
2014-03-09 13:53:38 -0700

    Adding xmonad and xmobar updates

--- modules/x11/files/xmonad.hs
+++ modules/x11/files/xmonad.hs
@@ -1,7 +1,10 @@
 import XMonad
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
-
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.DynamicLog
+import XMonad.Actions.CycleWS
+import XMonad.Util.EZConfig
 
 -- LAYOUTS
 import XMonad.Layout.Spacing
@@ -13,11 +16,18 @@ import XMonad.Layout.Grid
 --------------------------------------------------------------------------------------------------------------------
 -- DECLARE WORKSPACES RULES
 --------------------------------------------------------------------------------------------------------------------
-myLayout = avoidStruts (tiledSpace ||| grid ||| tiled)
+myLayout = avoidStruts (tiledSpace        ||| 
+                        Mirror tiledSpace |||
+                        fullscreen        ||| 
+                        grid              ||| 
+                        Mirror grid       ||| 
+                        tiled             |||
+                        Mirror tiled)
 	where
 		tiled  		= spacing 5 $ ResizableTall nmaster delta ratio []
 		tiledSpace  	= spacing 60 $ ResizableTall nmaster delta ratio []
                 grid            = spacing 20 $ Grid
+                fullscreen      = noBorders Full
 		-- Default number of windows in master pane
 		nmaster = 1
 		-- Percent of the screen to increment when resizing
@@ -29,17 +39,26 @@ myLayout = avoidStruts (tiledSpace ||| grid ||| tiled)
 --------------------------------------------------------------------------------------------------------------------
 -- WORKSPACE DEFINITIONS
 --------------------------------------------------------------------------------------------------------------------
-myWorkspaces = ["1"]
+myWorkspaces = map show [1..10]
 
 main = do
-	xmonad $ defaultConfig
+	xmonad =<< myXmobar (defaultConfig
 		{ terminal              = myTerminal
                 , borderWidth		= 1
 		, layoutHook 		= smartBorders $ myLayout
 		, workspaces 		= myWorkspaces
                 , normalBorderColor     = color8
                 , focusedBorderColor    = color8
-		}
+                -- Hack to make java apps work
+                , startupHook           = setWMName "LG3D"
+		} `additionalKeys` [ ((mod1Mask, xK_Right), nextWS)
+                  , ((mod1Mask, xK_Left), prevWS)
+                  , ((mod1Mask .|. shiftMask, xK_Right), shiftToNext >> nextWS)
+                  , ((mod1Mask .|. shiftMask, xK_Left), shiftToPrev >> prevWS)
+                  , ((mod1Mask, xK_Down), moveTo Next EmptyWS)
+                  , ((mod1Mask, xK_Up), toggleWS)
+                  ])
+    
 
 myTerminal 	= "urxvtc"
 
@@ -61,3 +80,14 @@ color6=  "#4B5C5E"
 color14= "#556D70"
 color7=  "#504339"
 color15= "#9a875f"
+
+myXmobar conf = statusBar "xmobar" myXmobarPP toggleStrutsKey conf
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask1} = (modMask1, xK_b)
+
+myXmobarPP = defaultPP { ppCurrent = xmobarColor color9 "" . wrap "[" "]"
+                       , ppTitle   = xmobarColor color8 "" . shorten 110
+                       , ppLayout  = concat . map (" " ++) . map (take 2) . words
+                       , ppVisible = wrap "(" ")"
+                       , ppUrgent  = xmobarColor color12 color13
+                       }
